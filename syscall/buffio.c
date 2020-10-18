daa17d7f76aebc4392b29cd8cc6f5d19aba8d7e0
Sonam2502 <sonamjaiswal2502@gail.com>
2020-10-18 13:28:24 -0400

    file descriptor demo stdin,stdout, strerr

---
+++ syscall/buffio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+   
+   printf("Hello\n");
+   
+    return 0;
+}
+
+// no \n - write 1
+// \n - write 2
